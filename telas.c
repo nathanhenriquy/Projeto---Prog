74e413585132a06534a099db1b3e8b12a710951c
Nathan Henrique <nathanlucindosantos@gmail.com>
2023-11-04 11:34:58 -0300

    subindo algumas telas

---
+++ telas.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+void menu(){
+    system("cls");
+
+    printf("*********************\n");
+    printf("*                   *\n");
+    printf("*    MENU PRINCIPAL  *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+    printf("* 1 - Entrar         *\n");
+    printf("* 2 - Cadastrar-se   *\n");
+    printf("* 3 - Adm           *\n");
+    printf("* 4 - Sair            *\n");
+    printf("*********************\n");
+
+}
+
+void menu_adm(){
+    system("cls");
+
+    printf("*********************\n");
+    printf("*                   *\n");
+    printf("*    MENU DO ADM    *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+    printf("* 1 - Entrar         *\n");
+    printf("* 2 - Voltar         *\n");
+    printf("*********************\n");
+
+}
+
+void adm(){
+    system("cls");
+
+    printf("*********************\n");
+    printf("*                   *\n");
+    printf("*    MENU DO ADM    *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+    printf("* 1 - Remover CLiente*\n");
+    printf("* 2 - Listar CLientes*\n");
+    printf("* 2 - Consultar CLiente*\n");
+    printf("*********************\n");
+
+}
