19612dfa1198193b00f3b0edcd0ad404779e3499
Nathan Henrique <nathanlucindosantos@gmail.com>
2023-11-05 14:38:21 -0300

    arrumando a parte para adm

---
+++ adm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+// fazer aqui a parte de desativação da conta e também a da exlusão caso solicitado
+
+// a parte de consulta por cpf
+
+// a parte de listagem dos cadastrados
