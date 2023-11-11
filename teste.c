ebf61273530196261f7b147f6dd2b4786ba80894
haliskipaulo <paulohaliski50@protonmail.com>
2023-11-11 19:14:17 -0300

    verificador de cpf pronto

--- teste.c
+++ teste.c
@@ -1,81 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 
-int verificaCPF(int cpf[])
-{
+int verificaCPF(int cpf[]) {
     int i;                      // contador
     int d1, d2;                 // armazenam os digitos verificadores 1 e 2 para comparar ao cpf
     int soma1 = 0, soma2 = 0;   // guardam a soma entre os digitos do cpf com seus respectivos pesos
     int peso1 = 10, peso2 = 11; // necessarios para a atribuição de pesos para os digitos
 
-    for (i = 0; i < 9; i++)
-    {
+    for (i = 0; i < 9; i++) {
         soma1 += cpf[i] * peso1;
         peso1--;
     }
 
-    if ((soma1 % 11) == 0 || (soma1 % 11) == 1)
-    {
-        d1 = 0;
-    }
-    else
-    {
-        d1 = 11 - (soma1 % 11);
-    }
+    d1 = (soma1 % 11 < 2) ? 0 : 11 - (soma1 % 11);
 
-    //
-
-    if (cpf[9] == d1)
-    {
-        soma2 = 0;
-        peso2 = 11;
-        
-        for (i = 0; i < 10; i++)
-        {
-            soma2 += cpf[i] * peso2;
-            peso2--;
-        }
-
-        if ((soma2 % 11) == 0 || (soma2 % 11) == 1)
-        {
-            d2 = 0;
-        }
-        else
-        {
-            d2 = 11 - (soma2 % 11);
-        }
-
-        if (cpf[10] == d2)
-        {
-            printf("\ncpf valido\n");
-            return 0;
-        }
-        else 
-        {
-            printf("\ncpf invalido\n");
-            return 1;
-        }
-
-    }
-    else
-    {
-        printf("\no cpf é invalido\n");
+    if (cpf[9] != d1) {
         return 1;
     }
 
+    soma2 = 0;
+    peso2 = 11;
+
+    for (i = 0; i < 10; i++) {
+        soma2 += cpf[i] * peso2;
+        peso2--;
+    }
+
+    d2 = (soma2 % 11 < 2) ? 0 : 11 - (soma2 % 11);
+
+    return (cpf[10] != d2); // se for incorreto, vai retornar 1 e se for correto, 0
 }
 
-int main()
-{
+int main() {
     int cpf[11];
 
-    printf("digite um cpf\n");
-    scanf("%i", cpf);
+    printf("Digite o CPF (apenas números): ");
+    for (int i = 0; i < 11; i++) {
+        scanf("%1d", &cpf[i]);
+    }
 
-    verificaCPF(cpf);
-
-
+    if (verificaCPF(cpf) == 0) {
+        printf("\nCPF válido\n");
+    } else {
+        printf("\nCPF inválido\n");
+    }
 
     return 0;
 }
--- verificacpf.h
+++ verificacpf.h
@@ -5,66 +5,32 @@
 // ATENÇÃO: caso alguem q nao seja eu(paulo) vá implementar o verificador de cpf na main, coloquei a função com retorno 1,
 // para sinalizar que o cpf tava errado e tem q repetir o printf/scanf, mas se estiver correto ele retorna 0
 
-int verificaCPF(int cpf[])
-{
+int verificaCPF(int cpf[]) {
     int i;                      // contador
     int d1, d2;                 // armazenam os digitos verificadores 1 e 2 para comparar ao cpf
     int soma1 = 0, soma2 = 0;   // guardam a soma entre os digitos do cpf com seus respectivos pesos
     int peso1 = 10, peso2 = 11; // necessarios para a atribuição de pesos para os digitos
 
-    for (i = 0; i < 9; i++)
-    {
+    for (i = 0; i < 9; i++) {
         soma1 += cpf[i] * peso1;
         peso1--;
     }
 
-    if ((soma1 % 11) == 0 || (soma1 % 11) == 1)
-    {
-        d1 = 0;
-    }
-    else
-    {
-        d1 = 11 - (soma1 % 11);
-    }
+    d1 = (soma1 % 11 < 2) ? 0 : 11 - (soma1 % 11);
 
-    //
-
-    if (cpf[9] == d1)
-    {
-        soma2 = 0;
-        peso2 = 11;
-        
-        for (i = 0; i < 10; i++)
-        {
-            soma2 += cpf[i] * peso2;
-            peso2--;
-        }
-
-        if ((soma2 % 11) == 0 || (soma2 % 11) == 1)
-        {
-            d2 = 0;
-        }
-        else
-        {
-            d2 = 11 - (soma2 % 11);
-        }
-
-        if (cpf[10] == d2)
-        {
-            printf("\ncpf valido\n");
-            return 0;
-        }
-        else 
-        {
-            printf("\ncpf invalido\n");
-            return 1;
-        }
-
-    }
-    else
-    {
-        printf("\no cpf é invalido\n");
+    if (cpf[9] != d1) {
         return 1;
     }
 
+    soma2 = 0;
+    peso2 = 11;
+
+    for (i = 0; i < 10; i++) {
+        soma2 += cpf[i] * peso2;
+        peso2--;
+    }
+
+    d2 = (soma2 % 11 < 2) ? 0 : 11 - (soma2 % 11);
+
+    return (cpf[10] != d2); // se for incorreto, vai retornar 1 e se for correto, 0
 }
